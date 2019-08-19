c5183fb4afb23716cc7d6ea4f29598ac3338f840
Zakkaray <bloomzakk@gmail.com>
2019-08-19 16:20:53 +0300

    Navbar and products page creation

---
+++ components/NavigationBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { NavStyles } from "../static/stylesheets/style-components.js";
+
+const NavigationBar = () => {
+  return (
+    <NavStyles>
+      <Navbar className="border-underlined" bg="light" expand="md">
+        <Container>
+          <Navbar.Brand className="bigger-brand">Vegetables</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse  className="justify-content-end" id="responsive-navbar-nav">
+            <Nav>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </NavStyles>
+  );
+};
+
+export default NavigationBar;
